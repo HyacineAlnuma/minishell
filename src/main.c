d270221a08b30cfc798150e97342d909d6e76414
Sebastien Cros <secros@z2r9p7.42lyon.fr>
2025-04-16 19:20:45 +0200

    add: Now possible to do echo ls | ./minishell and act like bash

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: halnuma <halnuma@student.42lyon.fr>        +#+  +:+       +#+        */
+/*   By: secros <secros@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/13 11:45:00 by secros            #+#    #+#             */
-/*   Updated: 2025/04/14 12:56:10 by halnuma          ###   ########.fr       */
+/*   Updated: 2025/04/16 19:20:13 by secros           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,16 +43,14 @@ int	synthax_errror(char *str)
 	return (0);
 }
 
-void	minishell(char **envp)
+void	minishell(char **envp, t_list **env)
 {
 	char	*input;
-	t_list	**env;
 	t_exec	**command;
 	t_sink	*bin;
 
 	command = NULL;
 	bin = NULL;
-	env = lst_env(envp);
 	signal(SIGINT, sig_handler);
 	signal(SIGQUIT, SIG_IGN);
 	while (1)
@@ -74,14 +72,26 @@ void	minishell(char **envp)
 
 int	main(int ac, char **av, char **envp)
 {
+	char	*mini_str;
+	t_list	**env;
+
 	(void) av;
 	if (ac != 1)
 	{
 		write (2, "Error: Bad arguments\n", 21);
 		return (1);
 	}
+	env = lst_env(envp);
+	if (!isatty(STDIN_FILENO))
+	{
+		mini_str = fill_dishwasher(get_next_line(0), free, get_sink(NULL));
+		if (!mini_str)
+			return (2);
+		exec(parsing(mini_str, env, get_sink(NULL)), env, envp);
+		return (do_dishes(get_sink(NULL)), 0);
+	}
 	print_ascii();
-	minishell(envp);
+	minishell(envp, env);
 	rl_clear_history();
 	do_dishes(get_sink(NULL));
 }
