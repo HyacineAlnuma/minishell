8afb859be24636334035b8d4829817ba092cb86c
Sebastien Cros <secros@z3r11p2.42lyon.fr>
2025-04-25 11:21:55 +0200

    Final V3.3 removed hard coded path

--- src/exec/check_cmd.c
+++ src/exec/check_cmd.c
@@ -3,58 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   check_cmd.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: halnuma <halnuma@student.42lyon.fr>        +#+  +:+       +#+        */
+/*   By: secros <secros@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 09:32:09 by halnuma           #+#    #+#             */
-/*   Updated: 2025/04/25 09:32:08 by halnuma          ###   ########.fr       */
+/*   Updated: 2025/04/25 11:20:13 by secros           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-int	check_if_cmd_exists(char **paths, t_exec *cmd)
-{
-	char	*path;
-	int		i;
-
-	path = NULL;
-	i = -1;
-	while (paths[++i])
-	{
-		path = paths[i];
-		path = fill_dishwasher(ft_strjoin(path, "/"), free, get_sink(NULL));
-		path = fill_dishwasher(ft_strjoin(path, cmd->cmd), \
-		free, get_sink(NULL));
-		if (!path)
-		{
-			perror("malloc error");
-			return (2);
-		}
-		if (!access(path, F_OK))
-		{
-			cmd->path = path;
-			return (1);
-		}
-	}
-	return (0);
-}
-
-int	check_cmd_no_env(t_exec *cmd)
-{
-	char	*paths[10];
-
-	paths[0] = "/usr/local/sbin";
-	paths[1] = "/usr/local/bin";
-	paths[2] = "/usr/sbin";
-	paths[3] = "/bin";
-	paths[4] = "/usr/games";
-	paths[5] = "/usr/local/games";
-	paths[6] = "/snap/bin";
-	paths[7] = "/usr/bin";
-	paths[8] = "/sbin";
-	paths[9] = NULL;
-	return (check_if_cmd_exists(paths, cmd));
-}
 
 int	check_cmd_path(char *paths, char **path, int i, t_exec *cmd)
 {
@@ -122,12 +78,13 @@ int	check_cmd(t_exec *cmd, t_list **env)
 		return (3);
 	paths = find_node(env, "$PATH");
 	if (!paths)
-		return (check_cmd_no_env(cmd));
-	else
 	{
-		paths = ft_strdup(paths);
-		if (!paths)
-			return (4);
-		return (check_cmd_with_env(cmd, paths));
+		print_error(NULL, NULL, "PATH is not set");
+		return (5);
 	}
+	else
+		paths = ft_strdup(paths);
+	if (!paths)
+		return (4);
+	return (check_cmd_with_env(cmd, paths));
 }
